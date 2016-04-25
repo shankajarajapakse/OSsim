06b0de490310cc6414f5d8bc16e2df9f1ae8ac58
pushpithaDilhan <pushpitha.14@cse,mrt.ac.lk>
2016-04-24 23:57:40 -0700

    first commit

---
+++ src/ossim/OSSim.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ossim;
+
+/**
+ *
+ * @author Pushpitha
+ */
+public class OSSim {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+}
